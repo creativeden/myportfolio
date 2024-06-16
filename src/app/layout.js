40a4ef32e79d8d07cac069b726fe556baa59bcef
creativeden <eurodeny@gmail.com>
2024-06-16 22:53:58 +0700

    Styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,18 +6,20 @@ import Footer from '@/components/footer/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Next App',
-  description: 'Next.js starter app',
+    title: 'Next App',
+    description: 'Next.js starter app',
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <div className='container'>
+                    <Navbar />
+                    {children}
+                    <Footer />
+                </div>
+            </body>
+        </html>
+    )
 }
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,30 +1,58 @@
-import Link from "next/link"
+'use client'
+
+import { useState } from 'react'
+import styles from './links.module.css'
+import NavLink from "./navLink/navLink"
+
+const links = [
+    {
+        title: 'Home',
+        path: '/',
+    },
+    {
+        title: 'About',
+        path: '/about',
+    },
+    {
+        title: 'Contact',
+        path: '/contact',
+    },
+    {
+        title: 'Portfolio',
+        path: '/portfolio',
+    },
+]
 
 const Links = () => {
-    const links = [
-        {
-            title: 'Home',
-            path: '/',
-        },
-        {
-            title: 'About',
-            path: '/about',
-        },
-        {
-            title: 'Contact',
-            path: '/contact',
-        },
-        {
-            title: 'Portfolio',
-            path: '/portfolio',
-        },
-    ]
+    const [open, setOpen] = useState(false)
+
+    //TEMPORARY
+    const session = true
+    const isAdmin = true
 
     return (
-        <div>
-            {links.map((link=>(
-                <Link href={link.path} key={link.title}>{link.title}</Link>
-            )))}
+        <div className={styles.container}>
+            <div className={styles.links}>
+                {links.map((link=>(
+                    <NavLink item={link} key={link.title} />
+                )))}
+                {session ? (
+                    <>
+                        {isAdmin && <NavLink item={{title: 'Admin', path: '/admin'}} />}
+                        <button className={styles.logout}>Logout</button>
+                    </>
+                ) : (
+                    <NavLink item={{title: 'Login', path: '/login'}} />
+                )}
+            </div>
+            <button className={styles.menuButton} onClick={() => setOpen((prev) => !prev)}>Menu</button>
+            {
+                open && <div className={styles.mobileLinks}>
+                    {links.map((link)=>(
+                        <NavLink item={link} key={link.title} />
+                    ))}
+                </div>
+            }
         </div>
     )
 }
