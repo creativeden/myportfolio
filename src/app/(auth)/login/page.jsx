0b98f461e2bda44cbf2d775b8b6588c1af91e2e3
creativeden <eurodeny@gmail.com>
2024-06-25 19:17:10 +0700

    Login by Github

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,6 +1,12 @@
-const LoginPage = () => {
+import { handleGithubLogin } from "@/lib/action"
+
+const LoginPage = async () => {
     return (
-        <div>LoginPage</div>
+        <div>
+            <form action={handleGithubLogin}>
+                <button>Login with Github</button>
+            </form>
+        </div>
     )
 }
 
