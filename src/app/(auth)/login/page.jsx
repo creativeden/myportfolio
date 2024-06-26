f5b9d498e9e8c19ff0001e2b223640ccd14348ca
creativeden <eurodeny@gmail.com>
2024-06-26 15:55:52 +0700

    Login & Register forms

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,13 +1,19 @@
-import { handleGithubLogin } from "@/lib/action"
+import LoginForm from "@/components/loginForm/loginForm";
+import { handleGithubLogin } from "@/lib/action";
+import styles from "./login.module.css";
 
-const LoginPage = async () => {
-    return (
-        <div>
-            <form action={handleGithubLogin}>
-                <button>Login with Github</button>
-            </form>
-        </div>
-    )
-}
+const LoginPage = () => {
 
-export default LoginPage
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <form action={handleGithubLogin}>
+          <button className={styles.github}>Login with Github</button>
+        </form>
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
